f81410b069277f7797a6865b3bc75348fbe7600d
Andrii Kurdiumov <kant2002@gmail.com>
2022-04-13 19:09:06 +0600

    Add primitive parsing of variables

--- EngLang.Tests/VariableNameTests.cs
+++ EngLang.Tests/VariableNameTests.cs
@@ -6,13 +6,24 @@ namespace EngLang.Tests
     public class VariableNameTests
     {
         [Fact]
-        public void Test1()
+        public void DetectVariable()
         {
             var sentence = "an apple";
 
             var parseResult = EngLangParser.Parse(sentence);
 
             var expectedVariableName = "apple";
+            var variableReference = parseResult.VariableReferences[0];
+            Assert.Equal(expectedVariableName, variableReference.Name);
+        }
+        [Fact]
+        public void DetectVariableWithWhiteSpaces()
+        {
+            var sentence = "an red apple";
+
+            var parseResult = EngLangParser.Parse(sentence);
+
+            var expectedVariableName = "red apple";
             var variableReference = parseResult.VariableReferences[0];
             Assert.Equal(expectedVariableName, variableReference.Name);
         }
--- EngLang/EngLangParser.cs
+++ EngLang/EngLangParser.cs
@@ -1,12 +1,21 @@
 namespace EngLang
 {
     using System;
+    using System.Linq;
 
     public class EngLangParser
     {
         public static EngLangParseResult Parse(string sourceCode)
         {
-            throw new NotImplementedException();
+            var parts = sourceCode.Split(' ');
+            var result = new EngLangParseResult();
+            if (parts[0] == "an")
+            {
+                var variableName = string.Join(' ', parts.Skip(1));
+                result.VariableReferences.Add(new VariableReference() { Name = variableName });
+            }
+
+            return result;
         }
     }
 }
