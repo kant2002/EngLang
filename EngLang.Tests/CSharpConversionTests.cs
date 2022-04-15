b8f00b6e11f5ce105676b32079fb48c6dfca9b56
Andrii Kurdiumov <kant2002@gmail.com>
2022-04-15 16:05:03 +0600

    Add conversion to C#

---
+++ EngLang.Tests/CSharpConversionTests.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+
+namespace EngLang.Tests;
+
+public class CSharpConversionTests
+{
+    [Fact]
+    public void ConvertVariable()
+    {
+        var sentence = "the name is an apple.";
+        var parseResult = EngLangParser.Parse(sentence);
+        var converter = new CSharpConverter();
+
+        var result = converter.Convert(parseResult);
+
+        var expectedCode = "apple name;\r\n";
+        Assert.Equal(expectedCode, result);
+    }
+
+    [Fact]
+    public void ConvertVariableWithStringLiteral()
+    {
+        var sentence = "the greetings is an string equal to \"Hello\"";
+        var parseResult = EngLangParser.Parse(sentence);
+        var converter = new CSharpConverter();
+
+        var result = converter.Convert(parseResult);
+
+        var expectedCode = "string greetings = \"Hello\"";
+        Assert.Equal(expectedCode, result);
+    }
+
+    [Fact]
+    public void ConvertVariableWithIntLiteral()
+    {
+        var sentence = "the answer to all things is an number equal to 42";
+        var parseResult = EngLangParser.Parse(sentence);
+        var converter = new CSharpConverter();
+
+        var result = converter.Convert(parseResult);
+
+        var expectedCode = "number answer_to_all_things = 42";
+        Assert.Equal(expectedCode, result);
+    }
+}
---
+++ EngLang/CSharpConverter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+
+namespace EngLang;
+
+public class CSharpConverter
+{
+    public string Convert(SyntaxNode node)
+    {
+        switch (node)
+        {
+            case Statement statement:
+                return ConvertStatement(statement);
+            case Expression expression:
+                return ConvertExpression(expression);
+            case VariableDeclaration variableDeclaration:
+                return ConvertVariableDeclaration(variableDeclaration);
+            default:
+                throw new NotImplementedException();
+        }
+    }
+
+    private string ConvertVariableDeclaration(VariableDeclaration variableDeclaration)
+    {
+        var result = new StringBuilder();
+        result.Append(ConvertToIdentifier(variableDeclaration.TypeName.Name));
+        result.Append(' ');
+        result.Append(ConvertToIdentifier(variableDeclaration.Name));
+        if (variableDeclaration.Expression != null)
+        {
+            result.Append(" = ");
+            result.Append(Convert(variableDeclaration.Expression));
+        }
+
+        return result.ToString();
+    }
+
+    private string ConvertExpression(Expression expression)
+    {
+        switch (expression)
+        {
+            case IntLiteralExpression intLiteralExpression:
+                return intLiteralExpression.Value.ToString();
+            case StringLiteralExpression stringLiteralExpression:
+                return $"\"{stringLiteralExpression.Value}\"";
+            default:
+                throw new NotImplementedException();
+        }
+    }
+
+    private string ConvertStatement(Statement statement)
+    {
+        switch (statement)
+        {
+            case BlockStatement blockStatement:
+                StringBuilder builder = new();
+                foreach (var childStatement in blockStatement.Statements)
+                {
+                    builder.AppendLine(ConvertStatement(childStatement));
+                }
+
+                return builder.ToString();
+            case VariableDeclarationStatement variableDeclarationStatement:
+                var declaration = variableDeclarationStatement.Declaration;
+                return $"{Convert(declaration)};";
+            default:
+                throw new NotImplementedException();
+        }
+    }
+
+    private string ConvertToIdentifier(string name)
+    {
+        return name.Replace(' ', '_');
+    }
+}
