bf5be980e6ffc622ae8e6b6fa1f9b95bd1d68d9b
Andrii Kurdiumov <kant2002@gmail.com>
2022-10-04 16:48:49 +0800

    Parse sentences on Workshop website Blocked by https://github.com/curiosity-ai/catalyst

--- Workbench/Program.cs
+++ Workbench/Program.cs
@@ -3,11 +3,15 @@ using Blazorise.Bootstrap5;
 using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Security.Cryptography;
 using Workbench;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+var random = RandomNumberGenerator.Create();
+
+Catalyst.Models.English.Register(); // You need to pre-register each language (and install the respective NuGet Packages)
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services
