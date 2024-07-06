e6f43208d76447dfd329dc20014a95c0503ef50b
Andrii Kurdiumov <kant2002@gmail.com>
2024-07-06 11:40:36 +0500

    Remove default value

--- EngLang/VariableDeclaration.cs
+++ EngLang/VariableDeclaration.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace EngLang;
 
-public record VariableDeclaration(string Name, TypeIdentifierReference TypeName, Expression? Expression = null) : SyntaxNode
+public record VariableDeclaration(string Name, TypeIdentifierReference TypeName, Expression? Expression) : SyntaxNode
 {
     public override IEnumerable<SyntaxNode> Children =>
         Expression is null ? new SyntaxNode[] { TypeName } : new SyntaxNode[] { TypeName, Expression };
